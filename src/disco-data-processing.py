fab24faef6affbf88fbd07a5bdd64489ce901451
jennifertramsu <jennifer.tramsu@mail.mcgill.ca>
2021-05-17 15:26:29 -0400

    Debugged merge

--- src/disco-data-processing.py
+++ src/disco-data-processing.py
@@ -268,7 +268,7 @@ source_path = '../disco-data-processing/{}/*/data_tables_from_*'.format(global_o
 destination_path = '../disco-data-processing/{}'.format(merge_output_directory)
 
 # call data merging function and write complete dataset to file
-merged_dataset = merge(source_path, destination_path, merge_output_directory)
+merged_dataset = merge(source_path, destination_path)
 output_file_name = "merged_binding_dataset.xlsx"
 merged_dataset.to_excel(os.path.join(merge_output_directory, output_file_name))
 print("Data processing completed! Merged_binding_dataset.xlsx file available in the output directory under merged.")
