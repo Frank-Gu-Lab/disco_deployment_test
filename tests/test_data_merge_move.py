25d799bfe4d11ef85716cf5591f7371cf1c511de
jennifertramsu <jennifer.tramsu@mail.mcgill.ca>
2021-05-18 08:54:26 -0400

    Wrapped tests in classes

--- tests/test_data_merge_move.py
+++ tests/test_data_merge_move.py
@@ -16,67 +16,70 @@ from src.data_merging import move, merge
 
 # testing overall functionality
 
-def test_move():
-    
-    # SETUP
-    
-    src_path = ".\\test-files\\test_move\\*"
-    dst_path = ".\\test-files\\output"
-    
-    # grab file names from src
-    
-    directories = glob.glob(src_path)
-    filenames = [glob.glob("{}/*".format(dir)) for dir in directories]
-    
-    os.mkdir(dst_path)
+class TestMove:
 
-    try:
+    def test_move(self):
+    
+        # SETUP
+    
+        src_path = ".\\test-files\\test_move\\*"
+        dst_path = ".\\test-files\\output"
         
-        move(src_path, dst_path)
+        # grab file names from src
         
-        for i in range(len(filenames)):
-            for file in filenames[i]: # file = absolute path
-                filename = os.path.basename(file)
-                assert os.path.isfile(dst_path + "\\" + filename)
+        directories = glob.glob(src_path)
+        filenames = [glob.glob("{}/*".format(dir)) for dir in directories]
+        
+        os.mkdir(dst_path)
 
-    finally:
-        
-        # TEARDOWN
-        shutil.rmtree(dst_path)
+        try:
+            
+            move(src_path, dst_path)
+            
+            for i in range(len(filenames)):
+                for file in filenames[i]: # file = absolute path
+                    filename = os.path.basename(file)
+                    assert os.path.isfile(dst_path + "\\" + filename)
+
+        finally:
+            
+            # TEARDOWN
+            shutil.rmtree(dst_path)
 
 ################## TESTING MERGE ####################
       
 # testing overall functionality
 
-def test_merge():
+class TestMerge:
+    def test_merge(self):
     
-    src_path = ".\\test-files\\test_merge\\data\\KHA\\data_tables_from_KHA"
-    dst_path = ".\\test-files\\test_merge\\output"
-    merge_path = ".\\test-files\\test_merge\\actual"
-    output_file_name = "merged_binding_dataset.xlsx"
-    
-    if not os.path.exists(merge_path):
-        os.mkdir(merge_path)
+        src_path = ".\\test-files\\test_merge\\data\\KHA\\data_tables_from_KHA"
+        dst_path = ".\\test-files\\test_merge\\output"
+        merge_path = ".\\test-files\\test_merge\\actual"
+        output_file_name = "merged_binding_dataset.xlsx"
+        
+        if not os.path.exists(merge_path):
+            os.mkdir(merge_path)
 
-    try:
+        try:
 
-        output = merge(src_path, dst_path)
-        output.to_excel(os.path.join(merge_path, output_file_name))
-        
-        actual = pd.read_excel(merge_path + "\\" + output_file_name)
-        expected = pd.read_excel(".\\test-files\\test_merge\\expected\\merged_binding_dataset.xlsx")
+            output = merge(src_path, dst_path)
+            output.to_excel(os.path.join(merge_path, output_file_name))
+            
+            actual = pd.read_excel(merge_path + "\\" + output_file_name)
+            expected = pd.read_excel(".\\test-files\\test_merge\\expected\\merged_binding_dataset.xlsx")
 
+            
+            # check if file is as expected
+            msg2 = "{} does not contain all the expected information.".format(merge_path + "\\" + output_file_name)
+            assert compare_excel(actual, expected), msg2
         
-        # check if file is as expected
-        msg2 = "{} does not contain all the expected information.".format(merge_path + "\\" + output_file_name)
-        assert compare_excel(actual, expected), msg2
-       
-    finally:
-        
-        # TEARDOWN
-        
-        # check if file exists
-        msg1 = "{} was not successfully created.".format(merge_path + "\\" + output_file_name)
-        assert os.path.exists(merge_path + "\\" + output_file_name), msg1
-        
-        os.remove(merge_path + "\\" + output_file_name)
+        finally:
+            
+            # TEARDOWN
+            
+            # check if file exists
+            msg1 = "{} was not successfully created.".format(merge_path + "\\" + output_file_name)
+            assert os.path.exists(merge_path + "\\" + output_file_name), msg1
+            
+            os.remove(merge_path + "\\" + output_file_name)
