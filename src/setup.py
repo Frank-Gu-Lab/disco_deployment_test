fa9a902fd584ce3040a4d3dca66b43f884d8c724
jennifertramsu <jennifer.tramsu@mail.mcgill.ca>
2021-08-16 11:06:30 -0400

    updated setup.py

--- src/setup.py
+++ src/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name="discoprocess",
     description="Python package to pre-process NMR data for machine learning applications.",
-    version='0.0.1',
+    author = ['Jeffrey Watchorn', 'Samantha Stuart', 'Jennifer Tram Su'],
     packages=['discoprocess'],
     install_requires=[
         'numpy', 
