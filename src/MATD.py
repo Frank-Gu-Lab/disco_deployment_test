3dd4f6484c824e2b0de0689c476fc6d63dd2c19d
hdmd2003 <67080763+hdmd2003@users.noreply.github.com>
2022-08-16 14:41:28 -0400

    Update MATD.py

--- src/MATD.py
+++ src/MATD.py
@@ -317,9 +317,11 @@ if choice == "Upload and analyze (Step 1)":
 
 
 
-        client = pymongo.MongoClient("mongodb+srv://GuDataScience:<password>@discojam.md7mbjs.mongodb.net/?retryWrites=true&w=majority", server_api=ServerApi('1'))
-        db = client.test
+        client = pymongo.MongoClient("mongodb+srv://GuDataScience:ek6M0iSpctfSMd4R@discojam.md7mbjs.mongodb.net/?retryWrites=true&w=majority", server_api=ServerApi('1'))
 
+        db = client["proton_binding_database"]
+
+        st.success(client.server_info())
 
 
 
